4c0f8b24f5dc893bbdc09c01b9654326a271c479
ramprakash <ramprakash.b.2015@gmail.com>
2021-03-04 19:59:24 -0800

    added rest of chapter 3

---
+++ GeoQuiz_Ch7/app/src/main/java/com/practice/android/geoquizchapter2/Question.java
@@ -0,0 +1,27 @@
+package com.practice.android.geoquizchapter2;
+
+public class Question {
+    private int mTextResId;         //Question Text
+    private boolean mAnswerTrue;    //Question Answer
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
